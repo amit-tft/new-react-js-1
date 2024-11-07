a3ed125f510bb211e5810ab7a083bf4559e3ab09
amit-tft <amitkumar953244@gmail.com>
2024-11-07 20:11:58 +0530

    Building basic layout of the app using component

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom/client';
 // const heading = React.createElement('h1', {id: 'heading'}, 'Heading content');
 // console.log(heading);
 
-const jsxHeading = (<h1 id="jsx" className='test'>
-    
-    This is the jsx element</h1>);  // JSX Element
+ // JSX Element
 
 // React Component: everything thing in react is component button, header , footer, card, search bar
 // There are two types of component class based component -old  way of creating component, it uses js classes to create component
@@ -16,29 +14,90 @@ const jsxHeading = (<h1 id="jsx" className='test'>
 // It is of making understand react otherwise it will throw an error
 // A functional compoent is normal js function which return some jsxs
 
-const f = "This is the content of a string variable"
+const Header = ()=> {
+    return(
+        <div id='header'>
+         <div id='logo'>
+        <img src='https://img.freepik.com/premium-vector/beautiful-unique-food-restaurant-company-logo-design_981150-2156.jpg'></img>
+         </div>
+         <div id='navigation'>
+           <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact</li>
+            <li>Cart</li>
+           </ul>
+         </div>
 
-const Fest = ()=> (
-    <h3> This is the content from the fest</h3>
-)
+        </div>
+    )
+}
 
-const Test = ()=>(
-   
-<div>
-<Fest/>
-<Fest> </Fest>
-{Fest()}
-{jsxHeading}("This statement is being printed using console.log")
-{console.log}
- <h2 className='th'> This is the h2</h2>
+const Card = ()=> {
+   return (
+     <div className='card'>
+     <div className='cardImg'>
+     <img src='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505'></img>
+     </div>
+     <div className='cardContent'>
+     <h2> Restaurant Name</h2>
+     <p> Description</p>
+     <h3> Price</h3>
+     <h4> 20 Minutes</h4>
+     </div>
+     </div>
+   )
 
-<h3 id='jk'> This is the h3</h3>
-{f}
-</div>
-)
+}
 
+
+const Body = ()=> {
+   return(
+     <div id='body'>
+        <div id='search'>
+
+        </div>
+        <div id='restro'>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+           <Card/>
+        </div>
+     </div>
+   )
+}
+
+const Footer = ()=> {
+    return(
+      <div id='footer'  style={{textAlign: "center"}}>
+        <h2> Copyright 2024</h2>
+      </div>
+    )
+}
+
+
+const AppLayout = ()=> {
+    return (
+         <div id="app">
+        <Header/>
+         <Body/>
+         <Footer/>
+         </div>
+    )
+}
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Test/>);
+root.render( <AppLayout/>);
